23d360d73e33c5f755f1d9616ad4ad878753a1c5
A Boechko <46904893+Aboechko@users.noreply.github.com>
2019-02-17 12:45:30 -0500

    Create Dictionary.py

---
+++ Dictionary.py
@@ -0,0 +1,10 @@
+//using dictionary
+
+capitals = {'Iowa':'DesMoines','Wisconsin':'Madison'}
+print(capitals['Iowa'])
+capitals['Utah']='SaltLakeCity'
+print(capitals)
+capitals['California']='Sacramento'
+print(len(capitals))
+for k in capitals:
+   print(capitals[k]," is the capital of ", k)
