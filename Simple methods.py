969c5f787fe6484bcfeda99f928077791300f913
A Boechko <46904893+Aboechko@users.noreply.github.com>
2019-03-16 20:51:55 -0400

    Create Simple methods.py

---
+++ Simple methods.py
@@ -0,0 +1,24 @@
+#Implement the simple methods getNum and getDen that will return the numerator and denominator of a fraction.
+
+class Fraction:
+  def __init__(self,top,bottom):
+
+    self.num = top
+    self.den = bottom
+
+  def __str__(self):
+    return str(self.num) + "/" + str(self.den)
+
+  def getNum(self):
+    return self.num
+
+  def getDen(self):
+    return self.den
+
+myfraction = Fraction(n,d)
+n = input("input numerator")
+d = input("input denominator")
+print(myfraction)
+
+print(myfraction.getNum())
+print(myfraction.getDen())
