f72b783ac05dfc78b6db3d59dcf381a5b39581d4
A Boechko <46904893+Aboechko@users.noreply.github.com>
2019-03-16 20:51:48 -0400

    Create Arithmetic operators.py

---
+++ Arithmetic operators.py
@@ -0,0 +1,12 @@
+#Implement the remaining simple arithmetic operators (__sub__, __mul__, and __truediv__).
+
+a = 10
+b = 5
+
+sub = a-b
+mul = a*b
+truediv = a//b
+
+print(sub)
+print(mul)
+print(truediv)
